0934546862566bae1f10bb554d77ebef33faee3e
Christian Czapka <christian.czapka@live.de>
2022-02-19 18:55:24 +0100

    added menu for program flow and updated readme

--- README.md
+++ README.md
@@ -1,9 +1,55 @@
 ### where2ski
 
+A little program to help you maximize your tan while skiing. Developed and tested with Python 3.6.9 under Ubuntu 18.04.
+
 ## Initial setup
+
+* Clone project to wherever you want to place it
+
+```sh
+git clone xxx
+```
 
 * (Optional) Create a virtual env
 
+You can verify that Python 3 is installed on your system by running:
+
 ```sh
-asd
+python3 -V
+```
+
+Let’s start by installing the python3-venv package that provides the venv module.
+
+```sh
+sudo apt install python3-venv
+```
+
+Switch to the directory where you would like to store your Python 3 virtual environments. Within the directory run the following command to create your new virtual environment:
+
+```
+python3 -m venv where2ski-env
+```
+
+The command above creates a directory called my-project-env, which contains a copy of the Python binary, the Pip package manager, the standard Python library and other supporting files.
+
+To start using this virtual environment, you need to activate it by running the activate script:
+
+```sh
+source where2ski-env/bin/activate
+```
+
+Once activated, the virtual environment’s bin directory will be added at the beginning of the $PATH variable. Also your shell’s prompt will change and it will show the name of the virtual environment you’re currently using.
+
+Now that the virtual environment is activated, we can start installing, upgrading, and removing packages using pip.
+
+* Install required packages.
+
+```sh
+pip install -r requirements.txt
+```
+
+* Execute program.
+
+```sh
+python3 where2ski.py
 ```
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import datetime
 
 class Forecast:
 	def __init__(self, resort, day, date, tmin_m, tmax_m, snowcm_m, tmin_v, tmax_v, snowcm_v, rrp, rrr, sun, snowl, wind):
@@ -19,9 +20,12 @@ class Forecast:
 		self.wind=wind
 
 
+def menu():
+	print()
+
 def get_forecast(resort, day):
 
-	print("getting weather for", resort)
+	print("... for", ((datetime.datetime.today()+datetime.timedelta(days=int(day))).strftime("%B %d, %Y")), resort)
 
 	html_text = requests.get('https://www.bergfex.at/' + resort + '/wetter/prognose/').text
 	soup = BeautifulSoup(html_text, 'lxml')
@@ -81,33 +85,53 @@ def display_full_forecast(resort, day_array):
 def get_sun_raw(fc):
 	return(fc.sun.strip('h'))
 
+
+
+## Initialization
+
+# add or remove resorts if necessary
 resorts = ['scheffau', 'kitzbuehel-kirchberg', 'nauders', 'serfaus-fiss-ladis', 'silvretta-arena-ischgl-samnaun', 'seefeld-rosshuette', 'wettersteinbahnen-ehrwald', 'stubaier-gletscher', 'hintertux', 'innsbruck-igls-patscherkofel', 'hochfuegen', 'zell-am-ziller', 'matrei', 'waidring-steinplatte']
+#resorts = ['scheffau', 'kitzbuehel-kirchberg', 'nauders', 'serfaus-fiss-ladis']
 forecasts = []
 
-day_array = [0,1,2,3,4,5,6,7,8]
-print(day_array)
+# range based on the 9-day forecast of bergfex
+day_array = ['0','1','2','3','4','5','6','7','8']
 
-forecast = get_forecast('scheffau', 0)
+## Programflow start
+
+day = input("\n!!MOIN!!\n\nWhich day from now do you wanna go skiing? Please enter a number between 0 (today) and 8: ")
+
+while day not in day_array:
+	day = input("\nWrong input. Please select a day in range 0 and 8: ")
+
+
+#forecast = get_forecast('scheffau', 0)
 #display_full_forecast(resorts[0], day_array)
 
+# get weather data for each resort
+print("\nGrabbing latest forecasts data...")
 for resort in resorts:
 	forecasts.append(get_forecast(resort, 7))
 
-for f in forecasts:
-	print(get_sun_raw(f),f.resort)
-	#print (int(f.sun.strip('h')))
+# only for debug
+#for f in forecasts:
+#	print(get_sun_raw(f),"h in",f.resort)
 
-
-#forecasts_sorted = sorted(forecasts, key=get_sun_raw(fo))
-
-
+# sort resort-forecasts by sun hours
 forecasts.sort(key=get_sun_raw, reverse=True)
 
-print("-------------")
-
-for f in forecasts:
-	print(get_sun_raw(f),f.resort)
+print("------------------------------------------------------")
+print("For", ((datetime.datetime.today()+datetime.timedelta(days=int(day))).strftime("%B %d, %Y")), "you can expect the following amount of sun hours for each of the resorts:")
+for index, item in enumerate(forecasts):
+	print("%i)	" %(index+1), get_sun_raw(item), "h in", item.resort)
 	#print (int(f.sun.strip('h')))
+print("------------------------------------------------------")
 
-#wichtig wären noch
-#
+option = int(input("Please select a resort number to view more detailed information. Press 0 to quit: "))
+while option != 0:
+	if option > 0 and option <= len(forecasts):
+		print("Showing detailed weather of", forecasts[option-1].resort)
+		display_day_forecast(forecasts[option-1])
+		option = int(input("Select other resort or press 0 to quit: "))
+	else:
+		option = int(input("Invalid option. Please select again: "))
